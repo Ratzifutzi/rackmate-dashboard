12af9c61df696cd54a3f7e2fd951d448a7015a05
Ratzifutzi <71138238+Ratzifutzi@users.noreply.github.com>
2025-08-08 21:38:24 +0200

    Moved components to the correct dir

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Topbar } from "./components/topbar";
+import { Topbar } from "../components/topbar";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
